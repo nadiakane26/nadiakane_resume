eb57f43ebd7b8c068b6a351eff0123fd3252bd81
Nadia Kane <nadia.kane26@gmail.com>
2025-02-24 18:30:05 -0600

    jQuery

--- resume-animations.js
+++ resume-animations.js
@@ -1,29 +1,13 @@
 // Toggle function by selecting the caret for more details of Experiences and Projects
 $('.toggle-icon').click(function () {
     const $sectionTitle = $(this).closest('.section-title');
-    let $sectionDesc;
+    const $sectionDesc = $sectionTitle.next('p').next('.section-desc').length ? 
+                         $sectionTitle.next('p').next('.section-desc') : 
+                         $sectionTitle.next('.section-desc');
 
-    // Check if there is a <p> tag present (Experiences)
-    if ($sectionTitle.next('p').length > 0) {
-        // If <p> exists, find the section description after <p>
-        $sectionDesc = $sectionTitle.next('p').next('.section-desc');
-    } else {
-        // If no <p> exists, directly find the section description (Projects)
-        $sectionDesc = $sectionTitle.next('.section-desc');
-    }
-
-    // Toggle the visibility of the section description
     $sectionDesc.slideToggle();
-
-    // Toggle the 'active' class on the clicked job title
     $sectionTitle.toggleClass('active');
-
-    // Check if the job title has the 'active' class and update the icon accordingly
-    if ($sectionTitle.hasClass('active')) {
-        $(this).text('▼');  // When expanded, arrow points down
-    } else {
-        $(this).text('▲');  // When collapsed, arrow points up
-    }
+    $(this).text($sectionTitle.hasClass('active') ? '▼' : '▲');
 });
 
 function toggleSidebar() {
